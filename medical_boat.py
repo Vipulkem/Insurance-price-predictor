1a8000f1e0b208684d5a0d9e87a0c2ef3177f705
Vipulkem <singhvipul169@gmail.com>
2025-11-20 18:48:58 +0530

    plotly version

--- medical_boat.py
+++ medical_boat.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import pickle
-import plotly.express as px
+# import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score,accuracy_score
